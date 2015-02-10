ff7e4a0cadeaf56cba96cf1f216e932a42703aca
Keagan Long <keaganlong@lawn-128-61-26-113.lawn.gatech.edu>
2015-02-10 13:12:09 -0500

    Forgot py

--- parser.py
+++ parser.py
@@ -1,24 +1,62 @@
-__author__ = 'alexstelea'
-
+from toposort import toposort
 import xml.etree.ElementTree as ET
+import json
 
-tree = ET.parse('EDWSTDIC_orders_XSD.xsd')
+
+tree = ET.parse('EDWSTDIC_orders_PHYSICAL.xml')
 root = tree.getroot()
+adjMap = {}
 
+def makeEntities():
+    for child in root:
+        if "element" in child.tag:
+            childName = child.attrib["name"]
+            dependencies = set()
+            for subChild in child:
+                subChildTag = subChild.tag
+                if "keyref" in subChildTag:
+                    for keyRefChild in subChild:
+                        if "selector" in keyRefChild.tag:
+                            dependencies.add(keyRefChild.attrib["xpath"][3:])
+            adjMap[childName] = dependencies
 
-def _recurse_xml_elements(node, depth=0):
+def dfs(graph):
+    jsonGraph = {}
+    jsonGraph["name"] = "root";
+    jsonGraph["children"] = []
+    visited = set()
+    for vertex in adjMap:
+        if vertex not in visited:
+            jsonGraph["children"].append(explore(vertex, visited))
+    return json.dumps(jsonGraph)
 
-    for child in node:
-        depth += 1
-        _recurse_xml_elements(child, depth=depth)
+def explore(parentName, visited):
+    parentDict = {}
+    parentDict["name"] = parentName
+    visited.add(parentName)
+    leaf = True
+    if parentName in adjMap:
+        for child in adjMap[parentName]:
+            if child not in visited:
+                leaf = False
+                if "children" not in parentDict:
+                    parentDict["children"] = []
+                parentDict["children"].append(explore(child,visited))
+    if leaf:
+        parentDict["size"] = 1994
+    return parentDict
 
-    if node.attrib:
-        print str(node.attrib) + " Depth:" + str(depth)
-
+def topologicalSort(graph):
+    topoList = list(toposort(graph))
+    dfs(topoList)
 
 def main():
-    _recurse_xml_elements(root)
-
+    makeEntities()
+    #topologicalSort(adjMap)
+    jsonGraph = dfs(adjMap)
+    print jsonGraph
+    with open("edw.json","w") as f:
+        f.write(jsonGraph)
 
 if __name__ == "__main__":
     main()
